dd2ae912a46eaf7e160827c153abe706239da3ea
raiszidane95 <raiszidane95@gmail.com>
2024-06-03 11:40:46 +0700

    Change Copyright name

--- resources/views/components/backend/shareds/bottom-bar.blade.php
+++ resources/views/components/backend/shareds/bottom-bar.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer text-center">
-    <strong>2024 &copy; <a href="/">PU-Net</a>.</strong>
-    All rights reserved - Develop By PionirTech
+    <strong>2024 &copy; <a href="/">Newus Technology</a>.</strong>
+    All rights reserved - Develop By Newus Technology
 </footer>
