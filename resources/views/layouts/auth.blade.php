43a4e644364fa6a7c166bcc268b106180771497e
raiszidane95 <raiszidane95@gmail.com>
2024-07-13 12:34:36 +0700

    Add Loader

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -17,10 +17,39 @@
     <link rel="stylesheet" href="{{ asset('admin') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('admin') }}/dist/css/adminlte.css">
+    <style>
+    body, html {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #f3f3f3;
+    }
+
+    #loading-screen {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+    }
+    </style>
 </head>
 
-<body class="hold-transition login-page vh-100">
-    @yield('main')
+<body class="login-page vh-100">
+    <div id="loading-screen">
+        <div class="loader"></div>
+    </div>
+    <div class="loader-btn"></div>
+    <div id="content" style="display: none;">
+        @yield('main')
+    </div>
     <!-- jQuery -->
     <script src="{{ asset('admin') }}/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
@@ -29,6 +58,29 @@
     <script src="{{ asset('admin') }}/dist/js/adminlte.min.js"></script>
     <!-- Laravel PWA -->
     <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        // loader script
+    window.addEventListener('load', function () {
+        const loadingScreen = document.getElementById('loading-screen');
+        const content = document.getElementById('content');
+
+        loadingScreen.style.display = 'none';
+        content.style.display = 'block';
+    });
+
+    const form = document.getElementById('form-login');
+    let textLogin = document.getElementById('text-login');
+    let btnLogin = document.getElementById('button-login')
+    const loader = document.getElementsByClassName('loader-btn');
+
+    console.log(form);
+    console.log(loader[0]);
+       form.addEventListener('submit', function(e) {
+            console.log('submit event on login')
+            btnLogin.removeChild(textLogin);
+            btnLogin.append(loader[0]);
+       })
+    </script>
     <script>
         if ("serviceWorker" in navigator) {
             // Register a service worker hosted at the root of the
